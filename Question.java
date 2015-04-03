6b112f3a1728821cc20a9f8fc420632c082fbff2
Luke Gerhart <lukegerhart@users.noreply.github.com>
2015-04-02 20:54:17 -0400

    Create Question.java

---
+++ Question.java
@@ -0,0 +1,97 @@
+/**
+ * Question class that represents a trivia question
+ * @author Luke Gerhart
+ * Date: 4/2/15
+ */
+public class Question {
+
+	private char rightAnswer;
+	private String[] answers;
+	private String question;
+	
+	public Question(String fromFile) {
+		rightAnswer = parseRightAnswer(fromFile);
+		answers = parseAnswers(fromFile);
+		question = parseQuestion(fromFile);
+	}
+	
+	/**
+	 * Extracts just the question from the string stored in the file.
+	 * @param questionString
+	 * @return The question
+	 */
+	private String parseQuestion(String questionString) {
+		String temp = "";
+		for (int i = 0; i < questionString.length(); i++) {
+			if (questionString.charAt(i) != '?') {
+				temp = temp.concat(String.valueOf(questionString.charAt(i)));
+			}
+			else {
+				temp = temp.concat("?");
+				break;
+			}
+		}
+		return temp;
+	}
+	
+	private String[] parseAnswers(String questionString) {
+		String[] answers = new String[4];
+		int startIndex = 0;
+		int endIndex = questionString.length()-2;
+		String trimmed = "";
+		for (int i = 0; i < questionString.length()-1; i++) {
+			if (questionString.charAt(i) == '?') {
+				startIndex = i + 2;
+				break;
+			}
+		}
+		trimmed = questionString.substring(startIndex, endIndex);
+		int[] startIndices = new int[4];
+		int counter = 0;
+		for (int i = 0; i < trimmed.length()-1; i++) {
+			if (trimmed.charAt(i) == ':') {
+				startIndices[counter] = i-1;
+				counter++;
+			}
+		}
+		int[] endIndices = new int[4];
+		for (int i = 0; i < 3; i++) {
+			endIndices[i] = startIndices[i+1] - 1;
+		}
+		endIndices[3] = trimmed.length();
+		for (int i = 0; i < 4; i ++) {
+			answers[i] = trimmed.substring(startIndices[i], endIndices[i]);
+		}
+		return answers;
+	}
+	
+	/**
+	 * Extracts the letter of the correct answer from the string stored in the file.<br>
+	 * In other words, it returns the last char of questionString.
+	 * @param questionString
+	 * @return letter of right answer
+	 */
+	private char parseRightAnswer(String questionString) {
+		return questionString.charAt(questionString.length()-1);
+	}
+	
+	/**
+	 * Returns the question.
+	 * @return question
+	 */
+	public String getQuestion() {
+		return question;
+	}
+	
+	/**
+	 * Returns the letter of the right answer.
+	 * @return rightAnswer
+	 */
+	public char getRightAnswer() {
+		return rightAnswer;
+	}
+
+	public String[] getAnswers() {
+		return answers;
+	}
+}
