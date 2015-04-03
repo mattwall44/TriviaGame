29eb0d64ae6cdb68ed5dd81d8980d5d254286edf
Luke Gerhart <lukegerhart@users.noreply.github.com>
2015-04-02 20:53:44 -0400

    Create Player.java
    Player class for players of the trivia game.

---
+++ Player.java
@@ -0,0 +1,53 @@
+/**
+ * Player class for the trivia game.
+ * 
+ * @author Luke Gerhart
+ * Date: 4/2/15
+ *
+ */
+public class Player {
+
+	private int points;
+	private int pin;
+	
+	/**
+	 * Constructor.
+	 * @param password Player's pin
+	 */
+	public Player(int password) {
+		points = 0;
+		pin = password;
+	}
+	
+	/**
+	 * Get how many points the user has.
+	 * @return points
+	 */
+	public int getPoints() {
+		return points;
+	}
+	
+	/**
+	 * Add one point to the player.
+	 */
+	public void addPoint() {
+		points++;
+	}
+	
+	/**
+	 * Get the player's pin.
+	 * @return pin
+	 */
+	public int getPin() {
+		return pin;
+	}
+	
+	/**
+	 * Checks if the user entered the right pin.
+	 * @param pin
+	 * @return true if its the right pin. False otherwise.
+	 */
+	public boolean checkPin(int pin) {
+		return (this.pin == pin);
+	}
+}
