daa87be8383287c5c9ffebf040a9a3270a7e6d88
Matthew Walheim <mattwall44@users.noreply.github.com>
2015-04-10 16:24:09 -0400

    Update Player.java
    Correct update for set/ get tieValue

--- Player.java
+++ Player.java
@@ -9,14 +9,16 @@ public class Player {
 
 	private int points;
 	private int pin;
-	
+	private String name;
+	private int tieValue;
 	/**
 	 * Constructor.
-	 * @param password Player's pin
 	 */
-	public Player(int password) {
+	public Player() {
+		pin = 0;
 		points = 0;
-		pin = password;
+		name = "";
+		tieValue=0;
 	}
 	
 	/**
@@ -49,5 +51,42 @@ public class Player {
 	 */
 	public boolean checkPin(int pin) {
 		return (this.pin == pin);
+	}
+	
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+	/**
+	 * Set method for tieValue
+	 * Used in tie break situations 
+	 * to determine who is closer to the correct answer
+	 * @param val (user's answer)
+	 */ 
+	public void setTieValue(int val){
+		tieValue=Math.abs(val);
+	}
+	
+	/**
+	 * Get method for tie value
+	 * @returns tieValue
+	 */
+	public int getTieValue(){
+		return tieValue;
+	}
+	
+	/**
+	 * Set the player's name
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Get method for the player's name
+	 * @return name
+	 */
+	public String getName() {
+		return name;
 	}
 }
